eb6ae2fd0bfd397d0afe2ac026d012b5663497bf
SynysterLove <synlgp@gmail.com>
2019-01-30 17:57:38 -0300

    Hago mas robusto el set de pruebas de la alineacion

--- Python/test/cases/test_alinear_senales.py
+++ Python/test/cases/test_alinear_senales.py
@@ -5,7 +5,7 @@ import numpy
 
 from src.core.provider.service_provider import ServiceProvider
 from src.domain.generadores_de_senales.generador_senoidal import GeneradorSenoidal
-from src.domain.senal_audio import SenalAudio
+from src.domain.senal_audio import SenalAudio, AlineacionException
 
 
 class AlinearSenalesTest(unittest.TestCase):
@@ -20,18 +20,18 @@ class AlinearSenalesTest(unittest.TestCase):
         fs = 5
         senal = SenalAudio(fs, [1, 0, 1, 2, 1])
         senal_a_truncar = SenalAudio(fs, [0, 0, 0, 1, 0, 1, 2, 1])
-
         senal_resultante = AlinearSenalesTest.operaciones_service.eliminar_delay_entre_senales(senal, senal_a_truncar)\
             .get_valores()
+
         self.assertListEqual(senal.get_valores(), senal_resultante)
 
     def test_que_alinee_correctamente_una_senal_y_su_version_con_pequeno_ruido_antes(self):
         fs = 5
         senal = SenalAudio(fs, [1, 0, 1, 2, 1])
         senal_a_truncar = SenalAudio(fs, [0.001, -0.001, 0.003, 1, 0, 1, 2, 1])
-
         senal_resultante = AlinearSenalesTest.operaciones_service.eliminar_delay_entre_senales(
             senal, senal_a_truncar).get_valores()
+
         self.assertListEqual(senal.get_valores(), senal_resultante)
 
     def test_que_alinee_correctamente_una_senal_de_10_segundos_y_su_version_con_pequeno_ruido_antes(self):
@@ -40,7 +40,26 @@ class AlinearSenalesTest(unittest.TestCase):
         senoide = GeneradorSenoidal().generar_senoide(fs, 10, frecuencia, 1, 0)
         ruido = [0.001, -0.001, 0.003, 0.002, 0.01]
         senoide_a_truncar = SenalAudio(fs, ruido + senoide.get_valores().copy())
-
         senal_resultante = AlinearSenalesTest.operaciones_service.eliminar_delay_entre_senales(
             senoide, senoide_a_truncar, 0.100).get_valores()
+
         self.assertListEqual(senoide.get_valores(), senal_resultante)
+
+    def test_que_no_modifique_senales_ya_alineadas(self):
+        fs = 44100
+        frecuencia = 200
+        senoide = GeneradorSenoidal().generar_senoide(fs, 10, frecuencia, 1, 0)
+        senoide_a_truncar = GeneradorSenoidal().generar_senoide(fs, 10, frecuencia, 1, 0)
+        senal_resultante = AlinearSenalesTest.operaciones_service.eliminar_delay_entre_senales(
+            senoide, senoide_a_truncar, 0.100).get_valores()
+
+        self.assertListEqual(senoide.get_valores(), senal_resultante)
+
+    def test_que_lance_excepcion_si_la_ventana_es_mas_larga_que_alguna_senal(self):
+        fs = 44100
+        frecuencia = 200
+        senoide = GeneradorSenoidal().generar_senoide(fs, 2, frecuencia, 1, 0)
+        senoide_a_truncar = GeneradorSenoidal().generar_senoide(fs, 2, frecuencia, 1, 0)
+
+        self.assertRaises(AlineacionException, AlinearSenalesTest.operaciones_service.
+                          eliminar_delay_entre_senales, senoide, senoide_a_truncar, 5)
