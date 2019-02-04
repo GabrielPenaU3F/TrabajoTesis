c69c50b2289d78d5ff8e3d2caeeca52d970e33f2
SynysterLove <synlgp@gmail.com>
2019-02-04 19:51:34 -0300

    Implemento la generación de la señal MLS

---
+++ Python/src/domain/generadores_de_senales/generador_mls.py
@@ -0,0 +1,24 @@
+import math
+
+import numpy
+
+from src.domain.senal_audio import SenalAudio
+
+
+class GeneradorMLS:
+
+    def generar_senal_mls(self, n_bits, periodos, fs):
+
+        valores = []
+        for i in range(periodos):
+            periodo = self.generar_periodo(n_bits)
+            valores += periodo
+        return SenalAudio(fs, valores)
+
+    def generar_periodo(self, n_bits):
+        largo_periodo = int(math.pow(2, n_bits) - 1)
+        senal = numpy.random.randint(2, size=largo_periodo)
+        for i in range(len(senal)):
+            if senal[i] == 0:
+                senal[i] = -1
+        return list(senal)
