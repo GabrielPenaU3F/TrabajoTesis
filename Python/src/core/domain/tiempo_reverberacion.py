e288b7ee9498a892c8e59b49dec22179812a226e
SynysterLove <synlgp@gmail.com>
2019-03-14 00:54:30 -0300

    Correccion a la interfaz gráfica y agregado de la clase TiempoReverberacion

---
+++ Python/src/core/domain/tiempo_reverberacion.py
@@ -0,0 +1,15 @@
+class TiempoReverberacion:
+
+    def __init__(self, rt, coef_correlacion, xi):
+        self.rt = rt
+        self.coef_correlacion = coef_correlacion
+        self.xi = xi
+
+    def get_rt(self):
+        return self.rt
+
+    def get_coef_correlacion(self):
+        return self.get_coef_correlacion()
+
+    def get_xi(self):
+        return self.xi
--- Python/src/view/vista_detallada/tab.py
+++ Python/src/view/vista_detallada/tab.py
@@ -78,8 +78,6 @@ class Tab(ABC):
 
         self.construir_frame_rts()
 
-        self.construir_frame_linealidad()
-
     def construir_seleccion_banda(self):
 
         self.frame_titulo_bandas = Label(self.frame_medicion)
@@ -88,99 +86,154 @@ class Tab(ABC):
         self.label_titulo_bandas_octava = Label(self.frame_titulo_bandas)
         self.label_titulo_bandas_octava.config(text=self.titulo_bandas_text, bg="#0c005a")
         self.label_titulo_bandas_octava.pack(ipadx=10, expand="True", fill="both")
-        self.frame_combobox_bandas = Frame(self.frame_medicion)
-        self.frame_combobox_bandas.config(height=20, bg="#5893d4")
-        self.frame_combobox_bandas.grid(row=1, column=0, sticky="nsew", padx=10, pady=(10, 0))
+        self.frame_medicion_bandas = Frame(self.frame_medicion)
+        self.frame_medicion_bandas.config(height=20)
+        self.frame_medicion_bandas.grid(row=1, column=0, sticky="nsew", padx=10, pady=(20, 0))
         bandas_estandar = self.obtener_bandas()
         self.banda_seleccionada = StringVar()
         self.banda_seleccionada.set(bandas_estandar[0])
-        self.combobox_banda = OptionMenu(self.frame_combobox_bandas, self.banda_seleccionada, *bandas_estandar)
-        self.combobox_banda.config(relief="groove", borderwidth=0, bg="#5893d4", activebackground="#0060ca")
+        self.combobox_banda = OptionMenu(self.frame_medicion_bandas, self.banda_seleccionada, *bandas_estandar)
+        self.combobox_banda.config(relief="groove", borderwidth=0, bg="#5893d4", activebackground="#0060ca", width=20)
         self.combobox_banda['menu'].config(bg="#5893d4", activebackground="#0060ca")
-        self.combobox_banda.pack(expand="True", fill="both")
+        self.combobox_banda.grid(row=0, column=0, padx=10)
         self.ponderacion_A_checked = BooleanVar(False)
-        self.checkbutton_ponderacion_A = Checkbutton(self.frame_medicion)
-        self.checkbutton_ponderacion_A.config(text="Ponderación A", variable=self.ponderacion_A_checked, selectcolor="#5e0606")
-        self.checkbutton_ponderacion_A.grid(row=2, column=0, pady=(10, 0), padx=10, sticky="w")
-        self.boton_calcular = Button(self.frame_medicion)
+        self.checkbutton_ponderacion_A = Checkbutton(self.frame_medicion_bandas)
+        self.checkbutton_ponderacion_A.config(text="Ponderación A", variable=self.ponderacion_A_checked,
+                                              selectcolor="#5e0606")
+        self.checkbutton_ponderacion_A.grid(row=0, column=1, padx=20)
+        self.boton_calcular = Button(self.frame_medicion_bandas)
         self.boton_calcular.config(text="Calcular", command=self.view.on_calcular, bg="#5e0606", width=20)
-        self.boton_calcular.grid(row=3, column=0, pady=(20, 10))
+        self.boton_calcular.grid(row=0, column=2, padx=10)
 
     def construir_frame_rts(self):
-        
-        self.frame_titulo_rts = Label(self.frame_medicion)
-        self.frame_titulo_rts.config(height=20, borderwidth=2, relief="groove")
-        self.frame_titulo_rts.grid(row=4, column=0, sticky="nsew", padx=10, pady=(15, 0))
-        self.label_titulo_rts = Label(self.frame_titulo_rts)
-        self.label_titulo_rts.config(text="Tiempos de reverberación", bg="#0c005a")
-        self.label_titulo_rts.pack(ipadx=10)
 
         self.frame_rts = Frame(self.frame_medicion)
-        self.frame_rts.grid(row=5, column=0, padx=10, pady=(15, 0))
+        self.frame_rts.config(borderwidth=2, relief='ridge')
+        self.frame_rts.grid(row=4, column=0, padx=10, pady=(15, 0))
 
-        self.label_edt = Label(self.frame_rts)
-        self.label_edt.config(text="EDT", bg="#5893d4", borderwidth=2, relief="groove", width=4)
-        self.label_edt.grid(row=0, column=0, pady=(0, 5))
-        self.edt_var = StringVar()
-        self.label_res_edt = Label(self.frame_rts)
-        self.label_res_edt.config(relief="sunken", bg="#becbff", borderwidth=2, width=10, textvariable=self.edt_var,
-                                  fg='black')
-        self.label_res_edt.grid(row=0, column=1, padx=(10, 0), pady=(0, 5))
-        self.label_t20 = Label(self.frame_rts)
-        self.label_t20.config(text="T20", bg="#5893d4", borderwidth=2, relief="groove", width=4)
-        self.label_t20.grid(row=1, column=0, pady=(0, 5))
-        self.t20_var = StringVar()
-        self.label_res_t20 = Label(self.frame_rts)
-        self.label_res_t20.config(relief="sunken", borderwidth=2, bg="#becbff", width=10, textvariable=self.t20_var,
-                                  fg='black')
-        self.label_res_t20.grid(row=1, column=1, padx=(10, 0), pady=(0, 5))
-        self.label_t30 = Label(self.frame_rts)
-        self.label_t30.config(text="T30", bg="#5893d4", borderwidth=2, relief="groove", width=4)
-        self.label_t30.grid(row=2, column=0, pady=(0, 5))
-        self.t30_var = StringVar()
-        self.label_res_t30 = Label(self.frame_rts)
-        self.label_res_t30.config(relief="sunken", borderwidth=2, bg="#becbff", width=10, textvariable=self.t30_var,
-                                  fg='black')
-        self.label_res_t30.grid(row=2, column=1, padx=(10, 0), pady=(0, 5))
-
-    def construir_frame_linealidad(self):
-
-        self.frame_titulo_linealidad = Label(self.frame_medicion)
+        self.frame_titulo_rts = Label(self.frame_rts)
+        self.frame_titulo_rts.config(height=20, borderwidth=2, relief="groove")
+        self.frame_titulo_rts.grid(row=0, column=0, sticky="nsew", padx=10, pady=10, columnspan=2)
+        self.label_titulo_rts = Label(self.frame_titulo_rts)
+        self.label_titulo_rts.config(text="Tiempos de reverberación", bg="#0c005a")
+        self.label_titulo_rts.pack(ipadx=10, expand="True", fill="both")
+        self.frame_titulo_linealidad = Label(self.frame_rts)
         self.frame_titulo_linealidad.config(height=20, borderwidth=2, relief="groove")
-        self.frame_titulo_linealidad.grid(row=6, column=0, sticky="nsew", padx=10, pady=(15, 0))
+        self.frame_titulo_linealidad.grid(row=0, column=2, sticky="nsew", padx=10, pady=10, columnspan=4)
         self.label_titulo_linealidad = Label(self.frame_titulo_linealidad)
-        self.label_titulo_linealidad.config(text="Linealidad", bg="#0c005a")
+        self.label_titulo_linealidad.config(text="Parámetros de linealidad", bg="#0c005a")
         self.label_titulo_linealidad.pack(ipadx=10, expand="True", fill="both")
 
-        self.frame_linealidad = Frame(self.frame_medicion)
-        self.frame_linealidad.grid(row=7, column=0, padx=10, pady=(15, 0))
+        self.construir_frame_edt()
+        self.construir_frame_t20()
+        self.construir_frame_t30()
+        self.construir_frame_curvatura()
 
-        self.label_r = Label(self.frame_linealidad)
-        self.label_r.config(text="r", bg="#5893d4", borderwidth=2, relief="groove", width=4)
-        self.label_r.grid(row=0, column=0, pady=(0, 5))
-        self.r_var = StringVar()
-        self.label_res_r = Label(self.frame_linealidad)
-        self.label_res_r.config(relief="sunken", bg="#becbff", borderwidth=2, width=10,
-                                       textvariable=self.r_var, fg='black')
-        self.label_res_r.grid(row=0, column=1, padx=(10, 0), pady=(0, 5))
+    def construir_frame_edt(self):
 
-        self.label_xi = Label(self.frame_linealidad)
-        self.label_xi.config(text="ξ", bg="#5893d4", borderwidth=2, relief="groove", width=4)
-        self.label_xi.grid(row=1, column=0, pady=(0, 5))
-        self.xi_var = StringVar()
-        self.label_res_xi = Label(self.frame_linealidad)
-        self.label_res_xi.config(relief="sunken", borderwidth=2, bg="#becbff", width=10,
-                                        textvariable=self.xi_var, fg='black')
-        self.label_res_xi.grid(row=1, column=1, padx=(10, 0), pady=(0, 5))
+        self.frame_edt = Frame(self.frame_rts)
+        self.frame_edt.grid(row=1, column=0, pady=(0, 10), columnspan=2)
+        self.label_edt = Label(self.frame_edt)
+        self.label_edt.config(text="EDT", bg="#5893d4", borderwidth=2, relief="groove", width=4)
+        self.label_edt.grid(row=0, column=0, padx=(0, 10), pady=10)
+        self.edt_var = StringVar()
+        self.label_res_edt = Label(self.frame_edt)
+        self.label_res_edt.config(relief="sunken", bg="#becbff", borderwidth=2, width=10, textvariable=self.edt_var,
+                                  fg='black')
+        self.label_res_edt.grid(row=0, column=1, padx=(10, 0), pady=10)
+        self.frame_linealidad_edt = Frame(self.frame_rts)
+        self.frame_linealidad_edt.grid(row=1, column=2, pady=(0, 10), columnspan=4)
+        self.label_r_edt = Label(self.frame_linealidad_edt)
+        self.label_r_edt.config(text="r", bg="#5893d4", borderwidth=2, relief="groove", width=4)
+        self.label_r_edt.grid(row=0, column=0, padx=10, pady=10)
+        self.r_edt_var = StringVar()
+        self.label_res_r_edt = Label(self.frame_linealidad_edt)
+        self.label_res_r_edt.config(relief="sunken", bg="#becbff", borderwidth=2, width=10, textvariable=self.r_edt_var,
+                                    fg='black')
+        self.label_res_r_edt.grid(row=0, column=1, padx=10, pady=10)
+        self.label_xi_edt = Label(self.frame_linealidad_edt)
+        self.label_xi_edt.config(text="ξ", bg="#5893d4", borderwidth=2, relief="groove", width=4)
+        self.label_xi_edt.grid(row=0, column=2, padx=10, pady=10)
+        self.xi_edt_var = StringVar()
+        self.label_res_xi_edt = Label(self.frame_linealidad_edt)
+        self.label_res_xi_edt.config(relief="sunken", bg="#becbff", borderwidth=2, width=10, fg='black',
+                                     textvariable=self.xi_edt_var)
+        self.label_res_xi_edt.grid(row=0, column=3, padx=10, pady=10)
 
-        self.label_curvatura = Label(self.frame_linealidad)
+    def construir_frame_t20(self):
+
+        self.frame_t20 = Frame(self.frame_rts)
+        self.frame_t20.grid(row=2, column=0, pady=(0, 10), columnspan=2)
+        self.label_t20 = Label(self.frame_t20)
+        self.label_t20.config(text="T20", bg="#5893d4", borderwidth=2, relief="groove", width=4)
+        self.label_t20.grid(row=0, column=0, padx=(0, 10), pady=10, sticky='w')
+        self.t20_var = StringVar()
+        self.label_res_t20 = Label(self.frame_t20)
+        self.label_res_t20.config(relief="sunken", borderwidth=2, bg="#becbff", width=10, textvariable=self.t20_var,
+                                  fg='black')
+        self.label_res_t20.grid(row=0, column=1, padx=(10, 0), pady=10, sticky='e')
+        self.frame_linealidad_t20 = Frame(self.frame_rts)
+        self.frame_linealidad_t20.grid(row=2, column=2, pady=(0, 10), columnspan=4)
+        self.label_r_t20 = Label(self.frame_linealidad_t20)
+        self.label_r_t20.config(text="r", bg="#5893d4", borderwidth=2, relief="groove", width=4)
+        self.label_r_t20.grid(row=0, column=2, padx=10, pady=10)
+        self.r_t20_var = StringVar()
+        self.label_res_r_t20 = Label(self.frame_linealidad_t20)
+        self.label_res_r_t20.config(relief="sunken", bg="#becbff", borderwidth=2, width=10, textvariable=self.r_t20_var,
+                                    fg='black')
+        self.label_res_r_t20.grid(row=0, column=3, padx=10, pady=10)
+        self.label_xi_t20 = Label(self.frame_linealidad_t20)
+        self.label_xi_t20.config(text="ξ", bg="#5893d4", borderwidth=2, relief="groove", width=4)
+        self.label_xi_t20.grid(row=0, column=4, padx=10, pady=10)
+        self.xi_t20_var = StringVar()
+        self.label_res_xi_t20 = Label(self.frame_linealidad_t20)
+        self.label_res_xi_t20.config(relief="sunken", bg="#becbff", borderwidth=2, width=10, fg='black',
+                                     textvariable=self.xi_t20_var)
+
+        self.label_res_xi_t20.grid(row=0, column=5, padx=10, pady=10)
+
+
+    def construir_frame_t30(self):
+        self.frame_t30 = Frame(self.frame_rts)
+        self.frame_t30.grid(row=3, column=0, pady=(0, 10), columnspan=2)
+        self.label_t30 = Label(self.frame_t30)
+        self.label_t30.config(text="T30", bg="#5893d4", borderwidth=2, relief="groove", width=4)
+        self.label_t30.grid(row=0, column=0, padx=(0, 10), pady=10, sticky='w')
+        self.t30_var = StringVar()
+        self.label_res_t30 = Label(self.frame_t30)
+        self.label_res_t30.config(relief="sunken", borderwidth=2, bg="#becbff", width=10, textvariable=self.t30_var,
+                                  fg='black')
+        self.label_res_t30.grid(row=0, column=1, padx=(10, 0), pady=10, sticky='e')
+        self.frame_linealidad_t30 = Frame(self.frame_rts)
+        self.frame_linealidad_t30.grid(row=3, column=2, pady=(0, 10), columnspan=4)
+        self.label_r_t30 = Label(self.frame_linealidad_t30)
+        self.label_r_t30.config(text="r", bg="#5893d4", borderwidth=2, relief="groove", width=4)
+        self.label_r_t30.grid(row=0, column=2, padx=10, pady=10)
+        self.r_t30_var = StringVar()
+        self.label_res_r_t30 = Label(self.frame_linealidad_t30)
+        self.label_res_r_t30.config(relief="sunken", bg="#becbff", borderwidth=2, width=10, textvariable=self.r_t30_var,
+                                    fg='black')
+        self.label_res_r_t30.grid(row=0, column=3, padx=10, pady=10)
+        self.label_xi_t30 = Label(self.frame_linealidad_t30)
+        self.label_xi_t30.config(text="ξ", bg="#5893d4", borderwidth=2, relief="groove", width=4)
+        self.label_xi_t30.grid(row=0, column=4, padx=10, pady=10)
+        self.xi_t30_var = StringVar()
+        self.label_res_xi_t30 = Label(self.frame_linealidad_t30)
+        self.label_res_xi_t30.config(relief="sunken", bg="#becbff", borderwidth=2, width=10, fg='black',
+                                     textvariable=self.xi_t30_var)
+        self.label_res_xi_t30.grid(row=0, column=5, padx=10, pady=10)
+
+    def construir_frame_curvatura(self):
+        self.frame_curvatura = Frame(self.frame_rts)
+        self.frame_curvatura.grid(row=4, column=2, pady=(0, 10), columnspan=4, sticky='e')
+        self.label_curvatura = Label(self.frame_curvatura)
         self.label_curvatura.config(text="C", bg="#5893d4", borderwidth=2, relief="groove", width=4)
-        self.label_curvatura.grid(row=2, column=0, pady=(0, 5))
+        self.label_curvatura.grid(row=0, column=0, padx=(0, 10), pady=10, sticky='w')
         self.curvatura_var = StringVar()
-        self.label_res_curvatura = Label(self.frame_linealidad)
+        self.label_res_curvatura = Label(self.frame_curvatura)
         self.label_res_curvatura.config(relief="sunken", borderwidth=2, bg="#becbff", width=10,
-                                               textvariable=self.curvatura_var, fg='black')
-        self.label_res_curvatura.grid(row=2, column=1, padx=(10, 0), pady=(0, 5))
+                                        textvariable=self.curvatura_var, fg='black')
+        self.label_res_curvatura.grid(row=0, column=1, padx=10, pady=10)
 
     def get_frecuencia_central_banda_seleccionada(self):
         return float(self.banda_seleccionada.get())
@@ -218,4 +271,5 @@ class Tab(ABC):
         self.edt_var.set(round(edt, 4))
         self.t20_var.set(round(t20, 4))
         self.t30_var.set(round(t30, 4))
+
 
