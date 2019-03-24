f8d1dfe4501afa42cb2fa1a04d2fd46e037ba09e
SynysterLove <synlgp@gmail.com>
2019-03-24 17:30:00 -0300

    Actualizo el archivo de requerimientos e implemento la funcionalidad de exportar archivos completa

--- Python/src/core/domain/archivos/escritor_de_archivos_ods_xls.py
+++ Python/src/core/domain/archivos/escritor_de_archivos_ods_xls.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict
 
-import pyexcel_ods3
+import pyexcel
 
 from core.domain.archivos.dialogo_exportar_medicion import DialogoExportarMedicion
 from core.domain.archivos.escritor_de_archivos import EscritorDeArchivos
@@ -20,23 +20,34 @@ class EscritorDeArchivosOdsXls(EscritorDeArchivos):
                 t20 = medicion.get_t20()
                 t30 = medicion.get_t30()
                 curvatura = medicion.get_curvatura()
-                data = OrderedDict()
-                hoja_ri = {"Respuesta impulsional": []}
-
-                hoja_ri["Respuesta impulsional"].append(["Datos muestreados a: ", ri.get_fs()])
-                hoja_ri["Respuesta impulsional"].append(["", "", ""])
-                valores_ri = ri.get_valores()
-                for i in range(65000):
-                    hoja_ri["Respuesta impulsional"].append([valores_ri[i]])
-                data.update(hoja_ri)
-                #data.update({"Curva de decaimiento": [["Datos muestreados a: ", cd.get_fs()],
-                 #                                     ["", "", ""],
-                  #                                    cd.get_valores()]})
-                #data.update({"Tiempo de reverberación": []})
-                pyexcel_ods3.save_data(archivo, data)
+                book = OrderedDict()
+                book["Respuesta impulsional"] = self.generar_sheet_senal(ri)
+                book["Curva de decaimiento"] = self.generar_sheet_senal(cd)
+                book["Tiempos de reverberación"] = self.generar_sheet_rts(edt, t20, t30, curvatura)
+                book_excel = pyexcel.get_book(bookdict=book)
+                book_excel.save_as(archivo.name)
 
             else:
                 raise IOException("No se pudo escribir el archivo")
 
+    def generar_sheet_senal(self, senal):
+        sheet = []
+        sheet.append(["Datos muestreados a: ", senal.get_fs()])
+        sheet.append([""])
+        valores_senal = senal.get_valores()
+        for i in range(len(valores_senal)):
+            sheet.append([valores_senal[i]])
+        return sheet
+
+    def generar_sheet_rts(self, edt, t20, t30, curvatura):
+        sheet = []
+        sheet.append(["", "RT", "r", "ξ"])
+        sheet.append([""])
+        sheet.append(["EDT", edt.get_rt(), edt.get_coef_correlacion(), edt.get_xi()])
+        sheet.append(["T20", t20.get_rt(), t20.get_coef_correlacion(), t20.get_xi()])
+        sheet.append(["T30", t30.get_rt(), t30.get_coef_correlacion(), t30.get_xi()])
+        sheet.append([""])
+        sheet.append(["Curvatura", curvatura])
+        return sheet
 
 
