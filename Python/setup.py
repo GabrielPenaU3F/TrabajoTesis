8b7ba34f599f4d2cca69b90d0b8b9a02fb03484b
SynysterLove <synlgp@gmail.com>
2019-03-27 17:58:02 -0300

    Agrego setup.py, MANIFEST y los inits

---
+++ Python/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='MedidorAcustico',
+    version='0.1',
+    author="Gabriel Pena",
+    author_email="gpena@untref.edu.ar",
+    description="Medidor Acustico",
+    url="https://github.com/GabrielPenaU3F/TrabajoTesis",
+    packages=setuptools.find_packages(),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license'
+)
