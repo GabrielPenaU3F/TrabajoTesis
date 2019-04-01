2bab903b679cbbc57ae39e2e206d43e9fe1c133f
Gabriel Pena <gpena@untref.edu.ar>
2019-04-01 01:55:31 -0300

    Agrego build.sh

---
+++ Python/build.sh
@@ -0,0 +1,29 @@
+export PYTHONPATH=medidor_acustico
+python3 -m PyInstaller "medidor_acustico/main.py" \
+--onefile \
+--name "medidor-acustico" \
+--clean \
+--specpath "Build" \
+--distpath "Build" \
+--hidden-import pyexcel.plugins \
+--hidden-import pyexcel.plugins.parsers \
+--hidden-import pyexcel.plugins.renderers \
+--hidden-import pyexcel.plugins.sources \
+--hidden-import pyexcel_io.readers.csvr \
+--hidden-import pyexcel_io.readers.csvz \
+--hidden-import pyexcel_io.readers.tsv \
+--hidden-import pyexcel_io.readers.tsvz \
+--hidden-import pyexcel_io.writers.csvw \
+--hidden-import pyexcel_io.readers.csvz \
+--hidden-import pyexcel_io.readers.tsv \
+--hidden-import pyexcel_io.readers.tsvz \
+--hidden-import pyexcel_io.database.importers.django \
+--hidden-import pyexcel_io.database.importers.sqlalchemy \
+--hidden-import pyexcel_io.database.exporters.django \
+--hidden-import pyexcel_io.database.exporters.sqlalchemy \
+--hidden-import pyexcel_xlsx \
+--hidden-import pyexcel_xlsx.xlsxr \
+--hidden-import pyexcel_xlsx.xlsxw \
+--hidden-import pyexcel_ods \
+--hidden-import pyexcel_ods.odsr \
+--hidden-import pyexcel_ods.odsw \
