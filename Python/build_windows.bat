45d73b00026c6fbbf47fef68fcae9207172286b9
Gabriel Pena <gpena@untref.edu.ar>
2019-04-11 03:50:38 -0300

    Agrego script de build para windows

---
+++ Python/build_windows.bat
@@ -0,0 +1,33 @@
+set PYTHONPATH=medidor_acustico
+
+python -m PyInstaller "medidor_acustico\main.py" ^
+--name "medidor-acustico" ^
+--clean ^
+--windowed ^
+--specpath "medidor_acustico" ^
+--distpath "executable" ^
+--hidden-import pyexcel.plugins ^
+--hidden-import pyexcel.plugins.parsers ^
+--hidden-import pyexcel.plugins.renderers ^
+--hidden-import pyexcel.plugins.sources ^
+--hidden-import pyexcel_io.readers.csvr ^
+--hidden-import pyexcel_io.readers.csvz ^
+--hidden-import pyexcel_io.readers.tsv ^
+--hidden-import pyexcel_io.readers.tsvz ^
+--hidden-import pyexcel_io.writers.csvw ^
+--hidden-import pyexcel_io.readers.csvz ^
+--hidden-import pyexcel_io.readers.tsv ^
+--hidden-import pyexcel_io.readers.tsvz ^
+--hidden-import pyexcel_io.database.importers.django ^
+--hidden-import pyexcel_io.database.importers.sqlalchemy ^
+--hidden-import pyexcel_io.database.exporters.django ^
+--hidden-import pyexcel_io.database.exporters.sqlalchemy ^
+--hidden-import pyexcel_xlsx ^
+--hidden-import pyexcel_xlsx.xlsxr ^
+--hidden-import pyexcel_xlsx.xlsxw ^
+--hidden-import pyexcel_ods ^
+--hidden-import pyexcel_ods.odsr ^
+--hidden-import pyexcel_ods.odsw ^
+--add-data "resources;.\resources"
+
+rmdir /Q /S build
