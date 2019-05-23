31a319a48b1ac86c7ff2beb58283520f774ce4eb
Gabriel Pena <gpena@untref.edu.ar>
2019-05-23 12:04:33 -0300

    Arreglo script de build de linux

--- Python/build_linux.sh
+++ Python/build_linux.sh
@@ -4,27 +4,26 @@ python -m PyInstaller 'medidor_acustico/main.py' \
 --clean \
 --specpath 'medidor_acustico' \
 --distpath 'executable' \
---hidden-import pyexcel.plugins \
---hidden-import pyexcel.plugins.parsers \
---hidden-import pyexcel.plugins.renderers \
---hidden-import pyexcel.plugins.sources \
---hidden-import pyexcel_io.readers.csvr \
---hidden-import pyexcel_io.readers.csvz \
---hidden-import pyexcel_io.readers.tsv \
---hidden-import pyexcel_io.readers.tsvz \
---hidden-import pyexcel_io.writers.csvw \
---hidden-import pyexcel_io.readers.csvz \
---hidden-import pyexcel_io.readers.tsv \
---hidden-import pyexcel_io.readers.tsvz \
---hidden-import pyexcel_io.database.importers.django \
---hidden-import pyexcel_io.database.importers.sqlalchemy \
---hidden-import pyexcel_io.database.exporters.django \
---hidden-import pyexcel_io.database.exporters.sqlalchemy \
+--hidden-import pyexcel \
+--hidden-import pyexcel.plugins.renderers.excel \
+--hidden-import pyexcel.plugins.renderers._texttable \
+--hidden-import pyexcel.plugins.parsers.excel \
+--hidden-import pyexcel.plugins.sources.http \
+--hidden-import pyexcel.plugins.sources.file_input \
+--hidden-import pyexcel.plugins.sources.memory_input \
+--hidden-import pyexcel.plugins.sources.file_output \
+--hidden-import pyexcel.plugins.sources.output_to_memory \
+--hidden-import pyexcel.plugins.sources.pydata.bookdict \
+--hidden-import pyexcel.plugins.sources.pydata.dictsource \
+--hidden-import pyexcel.plugins.sources.pydata.arraysource \
+--hidden-import pyexcel.plugins.sources.pydata.records \
+--hidden-import pyexcel_io \
+--hidden-import pyexcel_io.readers \
+--hidden-import pyexcel_io.writers \
+--hidden-import pyexcel_io.database \
 --hidden-import pyexcel_xlsx \
---hidden-import pyexcel_xlsx.xlsxr \
 --hidden-import pyexcel_xlsx.xlsxw \
 --hidden-import pyexcel_ods \
---hidden-import pyexcel_ods.odsr \
 --hidden-import pyexcel_ods.odsw \
 --add-data 'resources:./resources'
 
